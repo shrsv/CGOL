ee7f8a4a4cb55a3b8268bdf08908df689fe93d54
shrsv <shrijith.sv@gmail.com>
2015-12-11 20:03:34 +0530

    Init pygame

---
+++ cgol.py
@@ -0,0 +1,36 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+# set up the window
+DISPLAYSURF = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption("Conway's game of Life")
+
+# set up the colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# draw on the surface object
+DISPLAYSURF.fill(WHITE)
+pygame.draw.polygon(DISPLAYSURF, GREEN, ((146, 0), (291, 106), (236, 277), (0,
+    106)))
+
+pixObj = pygame.PixelArray(DISPLAYSURF)
+pixObj[480][380] = BLACK
+pixObj[482][382] = BLACK
+pixObj[484][384] = BLACK
+pixObj[486][386] = BLACK
+pixObj[488][388] = BLACK
+del pixObj
+
+# run the game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
